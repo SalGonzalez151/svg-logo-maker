768c6a9f30b2c50914dbbe528817a52910147016
Sal Gonzalez <imdone151@gmail.com>
2023-10-24 13:12:45 -0700

    added application function to write the SVG logo

--- index.js
+++ index.js
@@ -8,14 +8,28 @@ const questions = [{
 
 }, { type: 'input',
     name: 'textColor',
-    message: 'Please enter a color for the letters:'
+    message: 'Please enter a color for the letters:',
+    choices: ['']
 }, {
     type: 'list',
     name: 'shape',
     message:'Please a shape for the logo:',
-    choice: ['circle', 'triangle','square']
+    choices: ['circle', 'triangle','square']
 }, {
     type: 'input',
     name: 'logoColor',
     message:'Please enter a color for the logo:'
-}]
+}];
+
+function application () {
+    inquirer.prompt(questions).then(answers => {
+        try {
+            fs.writeFileSync("examples/logo.svg", shape(answers))
+            console.log("Success! Generated logo.svg")
+        } catch (error) {
+            console.log(error.message);
+        }
+    })
+}
+
+application();
